707c6c3abe33b62f320d0b390bac8808a0372f1d
Olena Kolesnik <lelya6350@gmail.com>
2026-04-01 19:18:37 +0300

    made mob menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,20 @@
+const openBtn = document.querySelector(".mobile-menu-open");
+const closeBtn = document.querySelector(".mobile-btn-close");
+const backdrop = document.querySelector(".backdrop");
+
+// відкрити меню
+openBtn.addEventListener("click", () => {
+  backdrop.classList.remove("hidden");
+});
+
+// закрити кнопкою
+closeBtn.addEventListener("click", () => {
+  backdrop.classList.add("hidden");
+});
+
+// закрити по кліку на фон
+backdrop.addEventListener("click", (e) => {
+  if (e.target === backdrop) {
+    backdrop.classList.add("hidden");
+  }
+});
